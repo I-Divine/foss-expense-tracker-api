bd7279409c2d31bd11c5d50e8ebfc24d3b090288
I-Divine <igwedinmaifechukwu@gmail.com>
2025-11-20 17:08:25 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
+  app.enableCors({
+    origin: true, // Reflects the request origin automatically
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
